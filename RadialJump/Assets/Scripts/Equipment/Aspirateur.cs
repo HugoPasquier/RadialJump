49a07da4696b80098986724c3295c0a1d3457dc6
hugoPasquier <hugo.pasquier6@gmail.com>
2022-11-17 11:53:21 +0100

    Première version rudimentaire de l'aspirateur

--- RadialJump/Assets/Scripts/Equipment/Aspirateur.cs
+++ RadialJump/Assets/Scripts/Equipment/Aspirateur.cs
@@ -16,10 +16,25 @@ public class Aspirateur : Weapon
         }
     }
 
+    Vector3 GrabPosition(){
+        return new Vector3(0, 0, 1) + Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0f));
+    }
+
     void Tir()
     {
         recoilSystem.RecoilFire(this);
         hand.KnockbackFire();
         currentCadence = 0;
+
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+        RaycastHit hit = new RaycastHit();
+        if (Physics.Raycast(ray, out hit, portee, canBeShot))
+        {
+            if (hit.collider.CompareTag("Aspirated"))
+            {
+                Debug.Log("Aspirated to : " + GrabPosition());
+                hit.collider.gameObject.transform.position = GrabPosition();
+            }
+        }
     }
 }
