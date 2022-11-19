a9c7fc596201a33addb8d4e5b0ffc206ddb3d8d0
Wesley Petit <wesley.petit.lemoine@gmail.com>
2022-11-19 14:54:14 +0100

    Ajout du spawn de plateforme avec yn raycast

--- RadialJump/Assets/Scripts/Equipment/PlatformAmmo.cs
+++ RadialJump/Assets/Scripts/Equipment/PlatformAmmo.cs
@@ -1,14 +1,43 @@
 using UnityEngine;
 
+/// <summary>
+/// Drop an platform when it's near his target
+/// </summary>
 public class PlatformAmmo : MonoBehaviour
 {
+    [SerializeField] private PlatformScalable _platformScalable;
+    [SerializeField] private float _targetSpawnRadius = 0.2f;
+    
     public Rigidbody Rigidbody;
-    [SerializeField] PlatformScalable _platformScalable;
+
+    private RaycastHit _targetHit;
+    private Transform _thisTransform;
     
-    
-    private void OnCollisionEnter(Collision other)
+    private void Awake()
     {
-        Instantiate(_platformScalable, other.contacts[0].point, Quaternion.identity);
+        _thisTransform = transform;
+
+        Ray ray = new Ray(_thisTransform.position, _thisTransform.forward);
+        if (!Physics.Raycast(ray, out _targetHit))
+        {
+            Debug.LogWarning("Can't create a platform in space.");
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        // Use distance to spawn a platform and not a collision event, because the ammos can't move too fast to detect a collision
+        // It pass through the other object
+        if (Vector3.Distance(_thisTransform.position, _targetHit.point) < _targetSpawnRadius)
+        {
+            CreatePlatform(_targetHit.point, _targetHit.normal);
+        }
+    }
+
+    private void CreatePlatform(Vector3 origin, Vector3 normal)
+    {
+        Instantiate(_platformScalable, origin, _platformScalable.transform.rotation);
         Destroy(gameObject);
     }
 }
