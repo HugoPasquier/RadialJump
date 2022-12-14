ffd4b4ef5435f55b8308bbed35c662dc228f9a12
Wesley Petit <wesley.petit.lemoine@gmail.com>
2022-12-14 10:36:41 +0100

    Ajout platform level

--- RadialJump/Assets/Scripts/Door.cs
+++ RadialJump/Assets/Scripts/Door.cs
@@ -20,7 +20,10 @@ public class Door : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (isOpen)
+        {
+            Open();
+        }
     }
 
     // Update is called once per frame
